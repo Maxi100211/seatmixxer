ba9a152efd7a2dcc770d9255722542d5d60a75e2
Maxi100211 <maximilianezban@gmail.com>
2025-09-04 12:01:19 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// Firebase init og fælles hjælpefunktioner
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
 
-// Din Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBMxiDa_hV95neKZFOndaWf-vW2OyA3I9c",
   authDomain: "seatmixxer.firebaseapp.com",
@@ -17,22 +15,14 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Hjælp: Tjek login, redirect til login hvis ikke logget ind
-export function requireLogin(redirect = "index.html") {
-  onAuthStateChanged(auth, user => {
-    if (!user) window.location.href = redirect;
-  });
-}
-
-// Hjælp: Gem data for bruger
 export async function saveUserData(data) {
-  if (!auth.currentUser) throw new Error("Ikke logget ind");
-  await setDoc(doc(db, "users", auth.currentUser.uid), data, { merge: true });
+  const user = auth.currentUser;
+  if (!user) throw new Error("Ikke logget ind");
+  await setDoc(doc(db, "users", user.uid), data, { merge: true });
 }
-
-// Hjælp: Hent data for bruger
 export async function loadUserData() {
-  if (!auth.currentUser) throw new Error("Ikke logget ind");
-  const snap = await getDoc(doc(db, "users", auth.currentUser.uid));
+  const user = auth.currentUser;
+  if (!user) throw new Error("Ikke logget ind");
+  const snap = await getDoc(doc(db, "users", user.uid));
   return snap.exists() ? snap.data() : {};
 }
